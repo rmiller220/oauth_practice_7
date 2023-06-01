df8c454afc98352f8724507d5bb4cb3f1b78030f
Reilly Miller <reillyjmiller31@gmail.com>
2023-06-01 12:07:17 -0600

    add routes for github oauth

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "home#index"
+  get '/auth/github/callback', to: 'sessions#create'
+  get '/dashboard', to: 'dashboard#show'
 end
